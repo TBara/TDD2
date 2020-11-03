223404a1baec43e1bfdf7e0d5d506d6b4aa1f1c4
tbara <tbarabasz@noblyapps.net>
2020-11-02 19:13:22 -0800

    Ensure one uppercase is present

--- check_pwd.py
+++ check_pwd.py
@@ -1,13 +1,16 @@
 def check_pwd(pwd):
     has_lower = False
+    has_upper = False
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     else:
         for x in pwd:
             if x.islower():
                 has_lower = True
+            elif x.isupper():
+                has_upper = True
 
-    if has_lower:
+    if has_lower and has_upper:
         return True
     else:
         return False
