4e1301a26ef3d847aab884807d73f701061e3cf5
tbara <tbarabasz@noblyapps.net>
2020-11-02 19:22:49 -0800

    Detect if at least one digit present

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,7 @@
 def check_pwd(pwd):
     has_lower = False
     has_upper = False
+    has_digit = False
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     else:
@@ -9,8 +10,10 @@ def check_pwd(pwd):
                 has_lower = True
             elif x.isupper():
                 has_upper = True
+            elif x.isdigit():
+                has_digit = True
 
-    if has_lower and has_upper:
+    if has_lower and has_upper and has_digit:
         return True
     else:
         return False
