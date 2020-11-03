28dd98f7c66ea8e500396dc523b5172a68cb2910
tbara <tbarabasz@noblyapps.net>
2020-11-02 18:38:31 -0800

    Project setup

---
+++ tests.py
@@ -0,0 +1,16 @@
+import unittest
+from check_pwd import check_pwd
+
+# Must be between 8 and 20 characters (inclusive)
+# Must contain at least one lowercase letter
+# Must contain at least one uppercase letter
+# Must contain at least one digit
+# Must contain at least one symbol from: ~`!@#$%^&*()_+-=
+
+
+class TestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
