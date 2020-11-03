55c1783dd75216be3b1b9f40d031f1533e08a677
tbara <tbarabasz@noblyapps.net>
2020-11-02 19:04:29 -0800

    Ensure one lowercase is present

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,14 @@
 def check_pwd(pwd):
+    has_lower = False
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     else:
+        for x in pwd:
+            if x.islower():
+                has_lower = True
+
+    if has_lower:
         return True
+    else:
+        return False
 
