e53bd9d8febbf0d0edd696bf94b5b9b4777263b4
tbara <tbarabasz@noblyapps.net>
2020-11-02 20:11:33 -0800

    Pass if one special character detected

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,11 @@
 def check_pwd(pwd):
+    sp_chars = ['~', '`', '!', '@', '#',
+                '$', '%', '^', '&', '*',
+                '(', ')', '_', '+', '-', '=']
     has_lower = False
     has_upper = False
     has_digit = False
+    has_chars = False
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     else:
@@ -12,9 +16,10 @@ def check_pwd(pwd):
                 has_upper = True
             elif x.isdigit():
                 has_digit = True
+            elif x in sp_chars:
+                has_chars = True
 
-    if has_lower and has_upper and has_digit:
+    if has_lower and has_upper and has_digit and has_chars:
         return True
     else:
         return False
-
